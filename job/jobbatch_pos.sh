a4f1154ab5835f7df856118622c212da20fc1b5b
liziwei <liziwei@mail.ustc.edu.cn>
2018-11-27 23:26:38 +0800

    change .sh files to commit job

--- job/jobbatch_pos.sh
+++ job/jobbatch_pos.sh
@@ -7,15 +7,15 @@ source /data2/R710liziwei/.bashrc
 #mkdir -p /data2/R710liziwei/g4/output/log
 #mkg4
 #cd ..
-for pos in -20 -10 0 10 20
+for pos in -12 -8 -4 0 
 do
 macname="$gwk/mac/cdor_pos$pos.mac"
 filename="pos$pos"
 condoname="g4batch.condo"
 echo $macname
 
-cp $gwk/mac/cdor_poschange.mac $macname
-sed -i "/position/s/z/$pos/" $macname
+cp $gwk/mac/cdor_pc.mac $macname
+sed -i "/centre/s/z/$pos/" $macname
 
 cp g4batch_template.condo $condoname
 sed -i "/Arguments/s/filename/$filename/" $condoname
--- job/tr_batch.sh
+++ job/tr_batch.sh
@@ -22,15 +22,15 @@ then
     if [ -d $path/$dirname ]
     then
         rootname=$path/$dirname/${dirname}
-        hadd ${rootname}data.root $path/$dirname/{0..39}data.root
-        hadd ${rootname}.root $path/$dirname/{0..39}.root
+        hadd ${rootname}data.root $path/$dirname/{0..79}data.root
+        hadd ${rootname}.root $path/$dirname/{0..79}.root
         root -b -q "MultiTiers_TACor_sep.C(\"${rootname}data\")"
     fi
     done
 else
     rootname=$path/$(basename $path)
-    hadd ${rootname}data.root $path/{0..39}data.root
-    hadd ${rootname}.root $path/{0..39}.root
+    hadd ${rootname}data.root $path/{0..79}data.root
+    hadd ${rootname}.root $path/{0..79}.root
     root -b -q "MultiTiers_TACor_sep.C(\"${rootname}data\")"
 fi
 
