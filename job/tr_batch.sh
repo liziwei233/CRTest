2399d25e8316eb0e2466dc28206e2b58c91bb0e2
liziwei <liziwei@mail.ustc.edu.cn>
2019-01-20 17:17:51 +0800

    add TAcor program for multi discriminated results

--- job/tr_batch.sh
+++ job/tr_batch.sh
@@ -22,14 +22,18 @@ then
     if [ -d $path/$dirname ]
     then
         rootname=$path/$dirname/${dirname}
-        hadd -f ${rootname}data.root $path/$dirname/{0..1599}data.root
+        #hadd -f ${rootname}data.root $path/$dirname/{0..1599}data.root
+        hadd -f ${rootname}dataFIX.root $path/$dirname/{0..1599}dataFIX.root
+        hadd -f ${rootname}dataCFD.root $path/$dirname/{0..1599}dataCFD.root
         hadd -f ${rootname}.root $path/$dirname/{0..1599}.root
         root -b -q "MultiTiers_TACor_sep.C(\"${rootname}data\")"
     fi
     done
 else
     rootname=$path/$(basename $path)
-    hadd -f ${rootname}data.root $path/{0..1599}data.root
+    #hadd -f ${rootname}data.root $path/{0..1599}data.root
+    hadd -f ${rootname}dataFIX.root $path/{0..1599}dataFIX.root
+    hadd -f ${rootname}dataCFD.root $path/{0..1599}dataCFD.root
     hadd -f ${rootname}.root $path/{0..1599}.root
     root -b -q "MultiTiers_TACor_sep.C(\"${rootname}data\")"
 fi
