2d00c6eac421089f2f7db66bee9420dbf382ce59
liziwei <liziwei@mail.ustc.edu.cn>
2019-03-05 19:22:14 +0800

    type: Modified; target file: Outputfun_MCP.C g4exe.sh geant4.condo rt.condo Config_Coz.gdml_entity cdorCR.mac aims: 1. Output the hittime of 1st pe, momentum direction and hit postion of cosmicray; 2. change the length of quartz and re-run.

--- job/g4exe.sh
+++ job/g4exe.sh
@@ -5,9 +5,9 @@ echo " ENV: "
 which root
 which geant4-config
 mkdir -p `dirname $3`
-./CRTest $1 $2 $3
+./CRTest $1 $2 $3 $4
 NAME=$3
-cluster=$4
+cluster=$5
 #echo $NAME > $PWD/name.log
 #root -b -q "MultiTiersOutputfun_SiPM.C(\"$NAME\")"
 
@@ -15,4 +15,4 @@ echo $NAME > ../job/name.log
 echo `date` >> $(dirname $3)/cluster.log
 echo -e "$cluster\n\n" >> $(dirname $3)/cluster.log
 #root -b -q "../job/MultiTiersOutputfun_SiPM.C(\"$NAME\",$thrd)"
-root -b -q "../job/Outputfun_MCP.C(\"$NAME\")"
+#root -b -q "../job/Outputfun_MCP.C(\"$NAME\")"
