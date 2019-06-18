1391bf3f31d191abc0e94d12a96030f8b572645f
liziwei <liziwei@mail.ustc.edu.cn>
2019-06-18 16:31:49 +0800

    Typed: Modified; Target files: job/*.sh & .condo; Aims: submmit job in R710;

--- job/g4exe.sh
+++ job/g4exe.sh
@@ -5,7 +5,15 @@ echo " ENV: "
 which root
 which geant4-config
 mkdir -p `dirname $3`
-./CRTest $@
+./CRTest $1 $2 $3 $4
 NAME=$3
-echo $NAME > $PWD/name.log
-root -b -q "MultiTiersOutputfun_SiPM.C(\"$NAME\")"
+cluster=$5
+#echo $NAME > $PWD/name.log
+#root -b -q "MultiTiersOutputfun_SiPM.C(\"$NAME\")"
+
+echo $NAME > ../job/name.log
+echo `date` >> $(dirname $3)/cluster.log
+echo -e "$cluster\n\n" >> $(dirname $3)/cluster.log
+#root -b -q "../job/MultiTiersOutputfun_SiPM.C(\"$NAME\",$thrd)"
+#root -b -q "../job/Outputfun_MCP.C(\"$NAME\")"
+root -b -q "../job/addBP.C(\"$NAME\")"
